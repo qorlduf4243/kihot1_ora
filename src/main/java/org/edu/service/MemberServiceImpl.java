985e87fb69f84534c025b8bce4c80299be7b9a61
hu-24 <hu-24@hu-24-PC>
2020-08-26 16:37:52 +0900

    kihot1-ora 싱크

--- src/main/java/org/edu/service/MemberServiceImpl.java
+++ src/main/java/org/edu/service/MemberServiceImpl.java
@@ -49,7 +49,15 @@ public class MemberServiceImpl implements IF_MemberService {
 
 	@Override
 	public List<MemberVO> testJobMethod() throws Exception {
-		//출력
+		List<MemberVO> list = memberDAO.testJobMethod();
+		//실행 상황 확인
+		System.out.println(list);
+		//6개월동안 회원정보 수정을 안한 회원을 휴면 계정으로 업데이트(아래)
+		System.out.println("===========6개월간 회원정보수정 안한 회원 휴면계정으로 처리");
+		for(MemberVO vo:list) {
+			vo.setEnabled(false);//휴면계정으로 SET변경
+			memberDAO.updateMember(vo);
+		}
 		return memberDAO.testJobMethod();
 	}
 
